60a09badb1aa90b3c6336ea4dd31d369729e8df5
Daniel Labios <labiosd@gmail.com>
2020-08-11 19:29:21 -0400

    Adventure lvl Done! See Prev. For Explorer lvl.

--- Program.cs
+++ Program.cs
@@ -14,9 +14,19 @@ namespace VariablesCS
             Console.WriteLine("cups of coffee:" + numberofCupsOfCoffee + "   " + "name:" + fullName + "   " + "today's date:" + today);
 
             //Practice Getting Input From User
-            Console.WriteLine("Please, give me your name:");
+            Console.WriteLine("Please give me your name:");
             var userName = Console.ReadLine();
-            Console.WriteLine($"Hello, {userName}! It is nice to meat you! Chop! Chop!");
+            if (userName == "Alice")
+            {
+                Console.WriteLine("Hello Alice, would you like to join our cannibal colony?");
+            }
+            else
+            {
+                Console.WriteLine($"Hello, {userName}! It is nice to meat you! Chop! Chop!");
+            }
+            /*Console.WriteLine("Please, give me your name:");
+            var userName = Console.ReadLine();
+            Console.WriteLine($"Hello, {userName}! It is nice to meat you! Chop! Chop!");*/
 
             //Practice Getting Different Types of Input From the User
             Console.WriteLine("Can you give me a number?: ");
@@ -33,7 +43,11 @@ namespace VariablesCS
             double difference = (secondOperand - firstOperand);
             double product = (firstOperand * secondOperand);
             double quotient = (firstOperand / secondOperand);
-            Console.WriteLine($"Thanks, {userName}. Fun Facts! If you have {firstOperand} bodies and add {secondOperand} bodies to the meatlocker, you get {sum} bodies! If you have {secondOperand} bodies in your meatlocker, and you eat {firstOperand} bodies, you are left with {difference} bodies. If you have {firstOperand} bodies, and you cut each one up into {secondOperand} pieces, you have {product} portion of body parts! Finally, if you have {firstOperand} bodies, and stash them into {secondOperand} meatlockers, each locker will have {quotient} bodies in them.");
+            Console.WriteLine($"Thanks, {userName}. Fun Facts! If you have {firstOperand} bodies and add {secondOperand} bodies to the"
+            + $" meatlocker, you get {sum} bodies! If you have {secondOperand} bodies in your meatlocker, and you eat {firstOperand} bodies"
+            + $", you are left with {difference} bodies. If you have {firstOperand} bodies, and you cut each one up into {secondOperand}"
+            + $" pieces, you have {product} pieces of body parts! Finally, if you have {firstOperand} bodies, and stash them into "
+            + $"{secondOperand} meatlockers, each locker will have {quotient} bodies in them.");
 
 
 
